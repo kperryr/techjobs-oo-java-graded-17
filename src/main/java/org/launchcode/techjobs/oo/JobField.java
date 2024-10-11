35d54852be859fc40df6ecd0b4b40c8019ebe4a7
Katherine <43319584+kperryr@users.noreply.github.com>
2024-10-10 21:22:41 -0400

    GA2

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,54 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+abstract public class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public JobField(String aValue){
+        this();
+        this.value = aValue;
+    }
+
+    // Custom toString, equals, and hashCode methods:
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField jobField)) return false;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
